9f68087795ed164a73eb214f626bd6fb5d176f69
Moulik Aggarwal <moulik@deepsource.io>
2020-12-01 17:05:13 +0530

    Added JS-0689 for autofix test
    Signed-off-by: Moulik Aggarwal <moulik@deepsource.io>

---
+++ index.vue
@@ -0,0 +1,15 @@
+<template>
+            <div
+              v-if="!visible"
+              v-for="item in items"
+              v-once
+              is="header"
+              v-on:click="functionCall"
+              ref="header"
+              :prop="headerData"
+              v-text="textContent"
+              id="uniqueID"
+              myProp="prop"
+              >
+            </div>
+          </template>
